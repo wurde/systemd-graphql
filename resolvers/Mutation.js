1c0273dd9f2a274fca16aef3c4a0a65fe5ab8045
Andy Bettisworth <andy@accreu.com>
2019-11-12 02:02:44 -0600

    Return status codes

--- resolvers/Mutation.js
+++ resolvers/Mutation.js
@@ -9,35 +9,11 @@ const child_process = require('child_process');
  */
 
 exports.start = (parent, args) => {
-  try {
-    console.log('args.pattern', args.pattern);
-    const result = child_process.spawnSync(
-      'systemctl',
-      ['start', args.pattern],
-      { encoding: 'utf8' }
-    );
-    console.log('result', result);
-    return true
+  const result = child_process.spawnSync(
+    'systemctl',
+    ['start', args.pattern],
+    { encoding: 'utf8' }
+  );
 
-    // const res = {};
-    // res['statusCode'] = result.status;
-
-    // const lines = result.stdout.split('\n');
-    // lines.forEach(line => {
-    //   if (line.match(/Loaded/)) {
-    //     res['loadState'] = line.trim().split(' ')[1];
-    //   }
-    //   if (line.match(/Active/)) {
-    //     res['activeState'] = line.trim().split(' ')[1];
-    //   }
-    //   if (line.match(/Main PID/)) {
-    //     res['mainPid'] = line.match(/\b\d+\b/)[0];
-    //   }
-    // })
-
-    // return res;
-  } catch (e) {
-    console.error('e', e);
-    return false
-  }
+  return result.status;
 };
