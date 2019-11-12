cecda845469edbcced14c70481df3f618395c9b6
Andy Bettisworth <andy@accreu.com>
2019-11-11 23:30:12 -0600

    Return Main PID for UnitStatus

--- resolvers/Query.js
+++ resolvers/Query.js
@@ -16,3 +16,19 @@ exports.version = () => {
   const result = child_process.execSync('systemctl --version', { encoding: 'utf8' });
   return result.match(/\b\d+\b/)[0];
 };
+
+exports.status = (parent, args) => {
+  try {
+    const result = child_process.execSync(`systemctl status ${args.pattern}`, { encoding: 'utf8' });
+    const lines = result.split('\n');
+    const res = {}
+    lines.forEach(line => {
+      if (line.match(/Main PID/)) {
+        res['mainPid'] = line.match(/\b\d+\b/)[0];
+      }
+    })
+    return res
+  } catch (e) {
+    console.error('e', e)
+  }
+};
