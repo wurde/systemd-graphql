7c157ad38ec8c2d74cf94fd74224c92dab159467
Andy Bettisworth <andy@accreu.com>
2019-11-12 02:05:18 -0600

    Add stop,reload,restart resolvers

--- resolvers/Mutation.js
+++ resolvers/Mutation.js
@@ -17,3 +17,33 @@ exports.start = (parent, args) => {
 
   return result.status;
 };
+
+exports.stop = (parent, args) => {
+  const result = child_process.spawnSync(
+    'systemctl',
+    ['stop', args.pattern],
+    { encoding: 'utf8' }
+  );
+
+  return result.status;
+};
+
+exports.reload = (parent, args) => {
+  const result = child_process.spawnSync(
+    'systemctl',
+    ['reload', args.pattern],
+    { encoding: 'utf8' }
+  );
+
+  return result.status;
+};
+
+exports.restart = (parent, args) => {
+  const result = child_process.spawnSync(
+    'systemctl',
+    ['restart', args.pattern],
+    { encoding: 'utf8' }
+  );
+
+  return result.status;
+};
