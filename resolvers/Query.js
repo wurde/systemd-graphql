d9332c963969349ef5337571185ed485334db8a0
Andy Bettisworth <andy@accreu.com>
2019-11-11 23:13:57 -0600

    Return current systemd version

--- resolvers/Query.js
+++ resolvers/Query.js
@@ -1,7 +1,18 @@
 /**
+ * Dependencies
+ */
+
+const child_process = require('child_process');
+
+/**
  * Define and export resolvers
  */
 
 exports.info = () => {
   return 'This is a GraphQL API for systemd.';
+};
+
+exports.version = () => {
+  const result = child_process.execSync('systemctl --version', { encoding: 'utf8' });
+  return result.match(/\b\d+\b/)[0];
 };
