c7f51adf2f1840ebbd250ebc64aa3aa5c73e9d60
Andy Bettisworth <andy@accreu.com>
2019-11-14 04:51:53 -0600

    Add slices to Query

--- resolvers/Query.js
+++ resolvers/Query.js
@@ -319,3 +319,29 @@ exports.timers = () => {
 
   return unitList;
 };
+
+exports.slices = () => {
+  const result = child_process.spawnSync(
+    'systemctl',
+    ['list-units', '--full', '--plain', '--no-legend', '--no-pager', '--type=slice'],
+    { encoding: 'utf8' }
+  );
+
+  const unitList = result.stdout
+    .trim()
+    .split('\n')
+    .map(unitLine => {
+      const cols = unitLine.split(/\s+/);
+      const type = path
+        .extname(cols[0])
+        .toUpperCase()
+        .replace('.', '');
+
+      return {
+        name: cols[0],
+        type: type
+      };
+    });
+
+  return unitList;
+};
