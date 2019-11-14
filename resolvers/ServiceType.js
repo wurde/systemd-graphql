3971ffbe564a0c4d6e32aedab91ce3eee8723299
Andy Bettisworth <andy@accreu.com>
2019-11-13 18:30:02 -0600

    Add resolvers for Enum types

---
+++ resolvers/ServiceType.js
@@ -0,0 +1,10 @@
+/**
+ * Define and export resolvers
+ */
+
+exports.simple  = () => 'SIMPLE';
+exports.forking = () => 'FORKING';
+exports.oneshot = () => 'ONESHOT';
+exports.notify  = () => 'NOTIFY';
+exports.dbus    = () => 'DBUS';
+exports.idle    = () => 'IDLE';
---
+++ resolvers/UnitType.js
@@ -0,0 +1,15 @@
+/**
+ * Define and export resolvers
+ */
+
+exports.service   = () => 'SERVICE';
+exports.socket    = () => 'SOCKET';
+exports.device    = () => 'DEVICE';
+exports.mount     = () => 'MOUNT';
+exports.automount = () => 'AUTOMOUNT';
+exports.swap      = () => 'SWAP';
+exports.target    = () => 'TARGET';
+exports.path      = () => 'PATH';
+exports.timer     = () => 'TIMER';
+exports.slice     = () => 'SLICE';
+exports.scope     = () => 'SCOPE';
