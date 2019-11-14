75989c2690178f07abf31bf8b53733714355a955
Andy Bettisworth <andy@accreu.com>
2019-11-14 04:44:46 -0600

    Add sockets to Query

--- resolvers/Query.js
+++ resolvers/Query.js
@@ -111,3 +111,29 @@ exports.services = () => {
 
   return unitList;
 };
+
+exports.sockets = () => {
+  const result = child_process.spawnSync(
+    'systemctl',
+    ['list-units', '--full', '--plain', '--no-legend', '--no-pager', '--type=socket'],
+    { encoding: 'utf8' }
+  );
+
+  const unitList = result.stdout
+    .trim()
+    .split('\n')
+    .map(unitLine => {
+      const cols = unitLine.split(/\s+/);
+      const type = path
+        .extname(cols[0])
+        .toUpperCase()
+        .replace('.', '');
+
+      return {
+        name: cols[0],
+        type: type
+      };
+    });
+
+  return unitList;
+};
